10d7a199a37e23ae3a05dfa175cfda33d1175178
Lester Collado Rolo <=>
2024-05-06 15:56:59 -0400

    add fields start, end, date

--- models/stamps.py
+++ models/stamps.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api
 
 class Stamps(models.Model):
     _name = 'stamps.stamps'
-    _description = 'Sellos'
+    _description = 'Sellos'  
     
     employee_ids = fields.Many2one(
         "hr.employee", 
@@ -19,7 +19,36 @@ class Stamps(models.Model):
         string="Cantidad",
         required=True)
 
-    @api.depends('value')
-    def _value_pc(self):
+    date = fields.Date(
+        string="Fecha"
+    )
+    
+    letter = fields.Char('Prefijo')
+    
+    stamps_start = fields.Integer('Número Sello Inicial')
+    
+    stamps_end = fields.Integer(
+        string= 'Número Sello Final',
+        compute='_compute_stamps_end'
+    )
+    
+    area = fields.Selection([
+        ('Tren', 'Tren'),
+        ('Muelle', 'Muelle'),
+        ('Salida', 'Salida'),
+        ('Explanada', 'Explanada'),
+    ], string='Área')
+            
+    @api.multi
+    @api.depends('employee_ids','employee_recibe_ids')
+    def name_get(self):
+        result = []
+        for account in self:
+            name = account.employee_ids.name + ' a ' + account.employee_recibe_ids.name
+            result.append((account.id, name))
+        return result
+    
+    @api.depends('stamps_start','amount_deliver')
+    def _compute_stamps_end(self):
         for record in self:
-            record.value2 = float(record.value) / 100
+            record.stamps_end = record.stamps_start + record.amount_deliver
